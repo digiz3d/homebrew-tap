d11aeda19d2d39af1d6d1fcfd0739f54cac51b5c
Léandre Daumont <1338620+digiz3d@users.noreply.github.com>
2026-02-03 22:35:54 +0100

    fix: syntax

--- Formula/gqlt.rb
+++ Formula/gqlt.rb
@@ -5,18 +5,18 @@ class Gqlt < Formula
 
   on_linux do
     on_arm do
-      url "https://github.com/digiz3d/graphql-parser/releases/download/v1.0.1/aarch64-linux-gqlt",
+      url "https://github.com/digiz3d/graphql-parser/releases/download/v1.0.1/aarch64-linux-gqlt"
       sha256 "462526415400f5e3b94c3c74692362dfbca0862255490855db4c72529a6dfd36"
     end
     on_intel do
-      url "https://github.com/digiz3d/graphql-parser/releases/download/v1.0.1/x86_64-linux-gqlt",
+      url "https://github.com/digiz3d/graphql-parser/releases/download/v1.0.1/x86_64-linux-gqlt"
       sha256 "b9b815c2da29bad446c8946b736b2ee9e57fb390f4cd800abb517af2132e9651"
     end
   end
 
   on_macos do
     on_arm do
-      url "https://github.com/digiz3d/graphql-parser/releases/download/v1.0.1/aarch64-macos-gqlt",
+      url "https://github.com/digiz3d/graphql-parser/releases/download/v1.0.1/aarch64-macos-gqlt"
       sha256 "159d2f8383bfcc7d2c374bfbab76045182021b4855e8054ebdfb3b5a7440ab31"
     end
   end
