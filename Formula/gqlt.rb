360cff2babf554968902027cb798c0bd08f3d232
Léandre Daumont <1338620+digiz3d@users.noreply.github.com>
2026-02-07 20:04:15 +0100

    fix: bump gqlt

--- Formula/gqlt.rb
+++ Formula/gqlt.rb
@@ -1,23 +1,27 @@
 class Gqlt < Formula
   desc "Simple GraphQL toolkit ! ❤"
-  homepage "https://github.com/digiz3d/graphql-parser"
-  version "1.0.1"
+  homepage "https://github.com/digiz3d/graphql-toolkit"
+  version "1.0.2"
 
   on_linux do
     on_arm do
-      url "https://github.com/digiz3d/graphql-parser/releases/download/v1.0.1/aarch64-linux-gqlt"
-      sha256 "462526415400f5e3b94c3c74692362dfbca0862255490855db4c72529a6dfd36"
+      url "https://github.com/digiz3d/graphql-toolkit/releases/download/v1.0.2/aarch64-linux-gqlt"
+      sha256 "5edddc98d0f89eba344841b3ef52154a2205cef75a871ec65eb86f0b8cbf82cc"
     end
     on_intel do
-      url "https://github.com/digiz3d/graphql-parser/releases/download/v1.0.1/x86_64-linux-gqlt"
-      sha256 "b9b815c2da29bad446c8946b736b2ee9e57fb390f4cd800abb517af2132e9651"
+      url "https://github.com/digiz3d/graphql-toolkit/releases/download/v1.0.2/x86_64-linux-gqlt"
+      sha256 "1e084115fdc37f08558c3fd8a9ad42e7e833ae7f11f0c6fb07d267973c4156dd"
     end
   end
 
   on_macos do
     on_arm do
-      url "https://github.com/digiz3d/graphql-parser/releases/download/v1.0.1/aarch64-macos-gqlt"
-      sha256 "159d2f8383bfcc7d2c374bfbab76045182021b4855e8054ebdfb3b5a7440ab31"
+      url "https://github.com/digiz3d/graphql-toolkit/releases/download/v1.0.2/aarch64-macos-gqlt"
+      sha256 "a9d4813802d6f49879cec15e636252cf3a3b52380fc0d5d22e9be994297385d9"
+    end
+    on_intel do
+      url "https://github.com/digiz3d/graphql-toolkit/releases/download/v1.0.2/x86_64-macos-gqlt"
+      sha256 "57660b7099ac8c5f032846ca5e2461895267f63af2bffa8cb29ed89150960541"
     end
   end
 
@@ -29,6 +33,7 @@ class Gqlt < Formula
     end
     on_macos do
       on_arm { binary_name = "aarch64-macos-gqlt" }
+      on_intel { binary_name = "x86_64-macos-gqlt" }
     end
     odie "Unsupported platform: no gqlt binary for #{OS.kernel_name}-#{Hardware::CPU.arch}" if binary_name.nil?
     bin.install binary_name => "gqlt"
